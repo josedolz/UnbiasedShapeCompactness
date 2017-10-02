b642ded061faa964dd9daeef7c7a0eee1f592bd9
Hoel Kervadec <hoel.kervadec.1@etsmtl.net>
2017-10-02 16:32:55 -0400

    [P] Fixed initial Graph Cut

--- python/energy.py
+++ python/energy.py
@@ -14,7 +14,7 @@ class Energy(object):
         self._was_minimized = False
 
     def set_neighbors(self, W):
-        W = scipy.sparse.coo_matrix(W)
+        W = sci.sparse.triu(W.tocoo())
 
         for i,j,w  in zip(W.row, W.col, W.data):
             self._g.add_edge(i, j, w, w)
@@ -29,7 +29,6 @@ class Energy(object):
                 self._g.add_tedge(i, u[1], u[0])
 
         self._prev_u = u.copy()
-        # self._was_minimized = True
 
     def minimize(self):
         e = self._g.maxflow()
