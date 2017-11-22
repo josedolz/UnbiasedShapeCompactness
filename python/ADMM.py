ec73c74d246a6dd83126863071a19ec556686bdc
Hoel Kervadec <hoel.kervadec.1@etsmtl.net>
2017-11-22 18:04:54 -0500

    Inverted label and pixel axis

--- python/ADMM.py
+++ python/ADMM.py
@@ -64,17 +64,17 @@ def compactness_seg_prob_map(img, prob_map, params=None):
     # Initial Graph Cut
     priors = prob_map.ravel()
 
-    unary_0 = np.zeros((N, 2))
-    unary_0[:, 0] = -np.log(ε + (1 - priors))
-    unary_0[:, 1] = -np.log(ε + priors)
+    unary_0 = np.zeros((2, N))
+    unary_0[0, :] = -np.log(ε + (1 - priors))
+    unary_0[1, :] = -np.log(ε + priors)
 
-    y_0, E, eg = graph_cut(params, W, unary_0, params._kernel, N)
+    y_0, E, eg = graph_cut(params, W, unary_0.T, params._kernel, N)
     seg_gc = y_0.reshape(img.shape)
 
     # ADMM
-    u = np.zeros((N, 2))
-    u[:, 0] = np.log(ε + 1 - prob_map.ravel())
-    u[:, 1] = np.log(ε + prob_map.ravel())
+    u = np.zeros((2, N))
+    u[0, :] = np.log(ε + 1 - prob_map.ravel())
+    u[1, :] = np.log(ε + prob_map.ravel())
     if params._init_graph_cut:
         y, metrics = admm(params, y_0, N, L, unary_0, u, W, eg, img)
     else:
@@ -86,11 +86,11 @@ def compactness_seg_prob_map(img, prob_map, params=None):
 
 
 # Careful with the order, since L is sparse. np.dot is unaware of that fact.
-c_length = lambda label, L: label[:, 1].T.dot(L.dot(label[:, 1]))
+c_length = lambda label, L: label[1, :].T.dot(L.dot(label[1, :]))
 
 
 def b_length(label, L):
-    seg = np.argmax(label, axis=1)
+    seg = np.argmax(label, axis=0)
     return seg.T.dot(L.dot(seg))
 
 
@@ -109,12 +109,12 @@ def admm(params, y_0, N, L, unary_0, u, W, eg, img):
 
     λ, μ1, μ2 = params._lambda, params._mu1, params._mu2
 
-    y = np.asarray([1-y, y]).T
+    y = np.asarray([1-y, y])
     unary = unary_0.copy()
-    z = np.zeros((N, 2))
+    z = np.zeros((2, N))
 
-    s = np.sum(y[:, 1])
-    ν1, ν2 = np.zeros((N, 2)), 0
+    s = np.sum(y[1, :])
+    ν1, ν2 = np.zeros((2, N)), 0
     tt = b_length(y, L)
 
     δ = np.ones((2, 2)) - np.diag((1,) * 2)
@@ -159,16 +159,16 @@ def admm(params, y_0, N, L, unary_0, u, W, eg, img):
         tt = b_length(y, L)
         # Update Lagrangian multipliers
         ν1 = ν1 + (y - z)
-        ν2 = ν2 + (s - np.sum(z[:, 1]))
+        ν2 = ν2 + (s - np.sum(z[1, :]))
         μ1 *= params._mu1Fact
         μ2 *= params._mu2Fact
 
-    return np.argmax(y, axis=1), metrics
+    return np.argmax(y, axis=0), metrics
 
 
 def update_metrics(params, y, L, metrics, i):
     l = b_length(y, L)
-    seg = np.argmax(y, axis=1)
+    seg = np.argmax(y, axis=0)
     area = seg.sum()
     compactness = l**2 / area
 
@@ -187,16 +187,16 @@ def update_z(params, λ, μ1, μ2, N, L, y, ν1, ν2, s, tt):
     α = (λ / s) * tt
 
     a = α*L + μ1 * sp.sparse.identity(N)
-    b = μ1 * (y[:, 1] + ν1[:, 1]) + μ2 * (s + ν2)
+    b = μ1 * (y[1, :] + ν1[1, :]) + μ2 * (s + ν2)
     if params._solvePCG:
         tmp = sp.sparse.linalg.cg(a, b)[0]
     else:
         tmp = sp.sparse.linalg.spsolve(a, b)
 
     const = (1 / μ1) * (1 / μ2 + N / μ1) ** -1
-    z = np.zeros((N, 2))
-    z[:, 1] = tmp - const * np.sum(tmp) * np.ones(N)
-    z[:, 0] = 1 - z[:, 1]
+    z = np.zeros((2, N))
+    z[1, :] = tmp - const * np.sum(tmp) * np.ones(N)
+    z[0, :] = 1 - z[1, :]
 
     return z
 
@@ -204,7 +204,7 @@ def update_z(params, λ, μ1, μ2, N, L, y, ν1, ν2, s, tt):
 def update_s(params, λ, μ2, ν2, tt, rr, z):
     β = .5 * λ * tt * rr
 
-    qq = np.sum(z[:, 1]) - ν2
+    qq = np.sum(z[1, :]) - ν2
 
     eq = [1, -qq, 0, -β/μ2]
     R = np.roots(eq)
@@ -219,25 +219,25 @@ def update_s(params, λ, μ2, ν2, tt, rr, z):
 
 
 def gc_update(params, unary, F, Λ, eg):
-    unary[:, 1] = F[:, 1].T / Λ
+    unary[1, :] = F[1, :] / Λ
     eg.set_unary(unary)
     _ = eg.minimize()
 
     y = np.zeros(unary.shape)
-    y[:, 1] = eg.get_labeling()
-    y[:, 0] = 1 - y[:, 1]
+    y[1, :] = eg.get_labeling()
+    y[0, :] = 1 - y[1, :]
 
     return y
 
 
 def crf_update(params, F, Λ, Φ, B, y, metrics):
     for j in range(params._crf_loops):
-        a = F + Λ * Φ.dot(y.ravel()).reshape(y.shape)
+        a = F + Λ * Φ.dot(y.T.ravel()).reshape(y.T.shape).T
 
         exp = np.exp(-a/B)
         y2 = y * exp
-        y2 = y2 / np.repeat(y2.sum(1), 2).reshape(y2.shape)
-        assert(np.allclose(y2.sum(1), 1))
+        y2 = y2 / np.tile(y2.sum(0), 2).reshape(y2.shape)
+        assert(np.allclose(y2.sum(0), 1))
         assert(0 <= y2.min() and y2.max() <= 1)
 
         if np.allclose(y2, y, atol=params._crf_tol):
